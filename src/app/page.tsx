13aca52ac34d111bfd85efee02bfee7b1a7ff968
ULYANA   IVANOVA <ulyanaivanova@macintosh-1.home>
2025-10-17 11:24:44 +0200

    выбор языка для админа

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 'use client';
+
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
 
 interface EventFormValues {
   title?: string;
